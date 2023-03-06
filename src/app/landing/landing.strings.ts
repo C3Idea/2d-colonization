cc9a8f3cdd5a54bdde5139f9841c17bbd04973bb
Abraham Solis <abraham.solis@c3consensus.com>
2023-03-06 13:50:20 -0800

    Added texts to Landing component

--- src/app/landing/landing.strings.ts
+++ src/app/landing/landing.strings.ts
@@ -1,10 +1,16 @@
 export class LandingStrings {
   static LabelTitle = "¿Cómo aparecen las venas en las hojas de los árboles?";
-  static LabelBody  = "Introduccion Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.";
+  static LabelBody  = "El algoritmo que vas a utilizar modela como las venas avanza \"colonizando\" la hoja al intentar acercarse a las fuentes de crecimiento: los atractores. Los parámetros involucrados son:";
+  static LabelBody2 = "1. Tamaño de paso: La velocidad a la que crecen las venas dentro de las hojas.";
+  static LabelBody3 = "2. Radio de atracción: Para cada fuente de hormona, o atractor, se define la distancia mas grande sobre la que tiene influencia.";
+  static LabelBody4 = "3. Radio de absorción: Una vez que las venas se acercan lo suficiente a los atractores, los absorben. Este parámetro define la distancia en la que esto ocurre.";
+  static LabelBody5 = "Cuando estos números cambian, el patrón resultante en la hoja cambia a su vez. Después de explorar como afecta cada parámetro te invitamos a dibujar tus propias hojas, puedes avanzar a la siguiente parte en la flecha a la derecha.";
   static AttractionRadius = "Radio de atracción";
   static AbsorptionRadius = "Radio de absorción";
   static StepLength       = "Tamaño de paso";
   static Close = "Cerrar";
   static IntroTitle = "¿Cómo aparecen las venas en las hojas de los árboles?";
   static IntroBody  = "¿Alguna vez has observado las venas de una hoja a contraluz? Los patrones que forman las venas en las hojsa son tan interesantes como diversos y han llamado la atención a biólogos y botánicos desde hace mucho tiempo. ¿Cómo es que aparecen? ¿por qué surgen figuras tan diferentes? Los factores involucrados que participan en el crecimiento de las plantas son abundantes y complejos. Al estudiar este fenómeno e intentar simplificarlo, los científicos desarrollaron una propuesta desde la colaboración con matemáticas y ciencias de la computación: ¡modelar el proceso con algoritmos!";
+  static IntroBody2 = "¿Qué es un algoritmo? Cuando escuchamos esta palabra pensamos en las recomendaciones musicales que nos hacen las plataformas en internet, o en los programas que hacen funcionar nuestros celulares y computadoras. Para los matemáticos, un algoritmo es una serie de instrucciones, ordenados con lógica que describen un proceso paso a paso, que nos permiten resolver un problema o una decisión.";
+  static IntroBody3 = "En este caso, los algoritmos que se utiliza para modelar las venas de las hojas toman en cuenta las hormonas de crecimiento en las hojas y como atraen la construcción de las venas, que transportan los nutrientes en las hojas. Con un pequeño conjunto de parámetros, estos algoritmos han sido utilizados para estudiar las plantas y ¡hasta para crear plantas en tus videojuegos favoritos! ¡Míralo tú mismo!";
 }
